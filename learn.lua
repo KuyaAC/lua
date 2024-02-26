39157586f97f7b001dd6c620b75e6d51687a7733
Allen Carl <aciesnet02@gmail.com>
2024-02-26 21:11:25 +0800

    learning lua

---
+++ learn.lua
@@ -0,0 +1,26 @@
+--Lets try to learn lua
+--Printing in lua
+print("Hello Lua!")
+
+--Variables
+
+local hi = "Hello"
+local name = "Allen Carl"
+
+print(hi)
+print(name)
+
+--functions
+local function greetings()
+    print("Greetings My Lord!")
+end
+
+greetings()
+
+--simple math equations
+local a = 2
+local b = 4
+local answer = a + b
+print(answer)
+
+--text input
